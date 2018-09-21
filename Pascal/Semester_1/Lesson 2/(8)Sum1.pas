1e5f2174e1013f6a4f5734fffd5753bc8634c353
Emisar <BeadsmanJudge@gmail.com>
2018-09-21 17:09:08 +0400

    Signed-off-by: Emisar <BeadsmanJudge@gmail.com>

--- Pascal/Semester_1/Lesson 2/(8)Sum1.pas
+++
@@ -1,8 +0,0 @@
-Var S:real;
-    n:integer;
-
-Begin
-  S:=0;
-  for n:=1 to 100 do S:=S + 1/n/n;
-  writeln(S)
-End.
--- Pascal/Semester_1/Lesson 9/ComplexVectors.pas
+++
@@ -1,6 +0,0 @@
-Uses Crt, Cplx;
-Var a: VectorC;
-Begin
-  ReadCVector(2, 2, a);
-  GoToXY(2, 3); Writeln(ModulVector(a));
-End.
