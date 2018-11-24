e9ad9391e46b734443774ff611ff1963354b371e
D-Flime <33832158+D-Flime@users.noreply.github.com>
2018-11-25 01:27:02 +0400

    Update generator.php

--- HeroesOfVM/application/mapGenerator/generator.php
+++ HeroesOfVM/application/mapGenerator/generator.php
@@ -1,78 +1,103 @@
 <?php
-
+const GENERATE_SAME_BIOMES = false;
 class Biome {
     public $type;
     public $x;
     public $y;
-
     public function __construct($x, $y, $type) {
         $this->type = $type;
         $this->x = $x;
         $this->y = $y;
     }
 }
-
 function createMap($width, $height, $biomesCount){
-    
     //Мапа
-    $map = [];
+    $map = []; //80*50
     for ($i = 0; $i < $width-1; $i++){
         $map[$i] = [];
     }
-
     //Массив биомов
     $biomes = [];
-    for ($i = 0; $i < $biomesCount-1; $i++){
-        $biomes[$i] = new Biome();
+    //Виды биомов
+    $biomesTypes = [];
+    $biomesTypes[0] = '==';
+    $biomesTypes[1] = '++';
+    $biomesTypes[2] = '--';
+    $biomesTypes[3] = '**';
+    $biomesTypes[4] = '//';
+    $biomesTypes[5] = '00';
+    $biomesTypes[6] = '11';
+    $biomesTypes[7] = '33';
+    $biomesTypes[8] = '44';
+    $biomesTypes[9] = '55';
+    $biomesTypes[10] = '66';
+    $biomesTypes[11] = '77';
+    $biomesTypes[12] = '88';
+    //Разделение на сегменты
+    $segments = [];
+    $count = $biomesCount; //10
+    $i = 0;
+    while (true){
+        $k = round(sqrt($count)); //3, 3, 2
+        $segments[$i] = $k;
+        $count -= $k; //7, 4, 2
+        $i++; //1, 2, 3
+        if ($count==2 || $count==3){
+            $segments[$i] = $count;
+            break;
+        }
     }
-
-    $biomes[0]->type = 'desert';
-    $biomes[0]->x = 5;
-    $biomes[0]->y = 5;
-
-    $biomes[1]->type = 'plains';
-    $biomes[1]->x = 10;
-    $biomes[1]->y = 5;
-
-    $biomes[2]->type = 'tundra';
-    $biomes[2]->x = 13;
-    $biomes[2]->y = 18;
-
-    $biomes[3]->type = 'swamp';
-    $biomes[3]->x = 26;
-    $biomes[3]->y = 26;
-
-    for ($i = 0; $i < $width-1; $i++) {
-        for ($j = 0; $j < $height-1; $j++) {
-            $nearest = '.'; // value here doesn't matter
-            $dist = 1000000; // select a big number
-
-            // walk over each biomes
+    //Удаляем счётчики
+    unset($i);
+    unset($count);
+    $segmentHeight = (int)($height / count($segments)); //12
+    for ($i = 0; $i < count($segments); $i++){ //0-4
+        $segmentWidth = (int)($width / $segments[$i]); //сегмент[0] 26*12
+        for ($j = 0; $j < $segments[$i]; $j++){
+            $biomeId = rand(0, count($biomesTypes)-1);
+            $biomes[] = new Biome(rand($segmentWidth*$j, $segmentWidth*($j+1)),
+                rand($segmentHeight*$i, $segmentHeight*($i+1)),
+                $biomesTypes[$biomeId]
+            );
+            if (!GENERATE_SAME_BIOMES) {
+                unset($biomesTypes[$biomeId]);
+                $biomesTypes = array_values($biomesTypes);
+            }
+        }
+    }
+    for ($i = 0; $i < $width; $i++) {
+        for ($j = 0; $j < $height; $j++) {
+            $nearest = '__';
+            $dist = 65535; // Большое число
+            // Проходимся по каждому биому
             for ($z = 0; $z < count($biomes); $z++) {
-
-                // calculate the difference in x and y direction
+                // Вычисляем разность в аправлении x и y
                 $xdiff = $biomes[$z]->x - $i;
                 $ydiff = $biomes[$z]->y - $j;
-
-                // calculate euclidean distance, sqrt is not needed
-                // because we only compare and do not need the real value
+                // Вычисляем евклидово расстояние(корень не нужен),
+                // потому что мы проводим сравнение и реальное значение не требуется
                 $cdist = $xdiff*$xdiff + $ydiff*$ydiff;
-
-                // is the current distance smaller than the old distance?
-                // if yes, take this biome
-                if ($cdist < $dist) {
+                // Новая дистанция меньше старой?
+                // Если да, берём этот биом
+                if ($cdist <= $dist) {
                     $nearest = $biomes[$z]->type;
                     $dist = $cdist;
                 }
             }
-
-            // set the map to the nearest biome       
+            // Присваиваем тайлу тип биома
             $map[$i][$j] = $nearest;
-
-            // you can mark the biome point with this code
-            // if (dist == 0) {
-            //    map[i][j] = 'X';
-            // }
         }
     }
+    
+    
+    return $map;
 }
+//$mapa = createMap(50, 50, 7);
+
+/*Это просто вывод, чтобы посмотреть, че получилось
+for ($i = 0; $i < 50; $i++) {
+    for ($j = 0; $j < 50; $j++) {
+        print_r($mapa[$i][$j]);
+    }
+    print_r('<br>');
+}*/
