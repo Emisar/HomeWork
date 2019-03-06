486501dfa209a21712159847de9cd63ee8c88105
Emisar <33164788+Emisar@users.noreply.github.com>
2019-03-06 17:25:26 +0400

    asd

--- Nodejs/application/modules/BaseModule.js
+++
@@ -1,8 +0,0 @@
-class BaseModule {
-    constructor(options) {
-        this.mediator = options.mediator;
-        this.TRIGGERS = this.mediator.getTriggers();
-        this.EVENTS = this.mediator.getEvents();
-    }
-}
-module.exports = BaseModule;
--- Nodejs/application/modules/user/User.js
+++
@@ -1,7 +0,0 @@
-class User {
-    constructor(options) {
-        this.nickname = options.nickname;
-        this.token = options.token;
-    }
-}
-module.exports = User;
