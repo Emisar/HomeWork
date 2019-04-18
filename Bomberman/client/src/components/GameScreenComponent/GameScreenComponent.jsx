18df17929b56dd8167505222d2b40278dae553ef
Shunskiy <pas4o@yandex.ru>
2019-04-18 19:27:37 +0400

    three.js test

--- Bomberman/client/src/components/GameScreenComponent/GameScreenComponent.jsx
+++ Bomberman/client/src/components/GameScreenComponent/GameScreenComponent.jsx
@@ -10,106 +10,99 @@ class GameScreenComponent extends Component {
         this.EVENT = props.socketEvent();
         
         this.socket.on(this.EVENT.UPDATE_SCENE, (data) => this.updateScene(data));
-
     }
 
+
+componentDidMount() {
+        this.width = 400;
+        this.height = 400;
+        this.scene = new THREE.Scene();
+            this.scene.background = new THREE.Color(0x222222);
+
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(this.width, this.height);
+        this.renderer.setClearColor(0xffffff, 1.0);
+
+        this.dom = document.getElementById('game-screen');
+        this.dom.appendChild(this.renderer.domElement);
+
+        this.camera = new THREE.PerspectiveCamera(100, this.width / this.height, 0.1, 1000);
+        this.camera.position.z = 100;
+        this.camera.position.y = -50;
+
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.enabled = true;
+        this.controls.maxDistance = 1500;
+        this.controls.minDistance = 0;
+
+        this.light_p = new THREE.PointLight(0xffffff);
+        this.light_p.position.set(10000, 10000, 10000);
+        this.scene.add(this.light_p);
+
+        this.light_a = new THREE.AmbientLight(0x333333);
+        this.scene.add(this.light_a);
+
+        this.animate = () => {
+            requestAnimationFrame(this.animate);
+            this.renderer.render(this.scene, this.camera);
+        }
+
+        this.animate();
+
+}
+
     updateScene(data) {
-//---------------------------------------- Подготовка работы Three.js
-        const width = 400;
-        const height = 400;
 
-        console.log(data);
+        console.log(data)
 
-        const scene = new THREE.Scene();
-            scene.background = new THREE.Color(0x222222);
 
-        const renderer = new THREE.WebGLRenderer();
-                renderer.setSize(width, height);
-                renderer.setClearColor(0xffffff, 1.0);
-
-        const dom = document.getElementById('game-screen');
-                dom.appendChild(renderer.domElement);
-
-        const camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 1000);
-                camera.position.z = 100;
-                camera.position.y = -50;
-
-        const controls = new OrbitControls(camera, renderer.domElement);
-                controls.enabled = true;
-                controls.maxDistance = 1500;
-                controls.minDistance = 0;
-
-//-----------------------------------------------------------------------------
-
-//-------------------------Работа со сценой
-        var addBlock = (x, y, z, color) => {
+        this.addBlock = (x, y, z, color) => {
             let geometry = new THREE.BoxGeometry(10, 10, 10);
             let material = new THREE.MeshLambertMaterial({color: color});
             let cube = new THREE.Mesh(geometry, material);
-            scene.add(cube);
+            this.scene.add(cube);
             cube.position.set(x, y, z);
         }
 
-        var addPlayer = (x, y) => {
-            let geometry = new THREE.CircleGeometry(10, 10);
+
+        this.addPlayer = (x, y) => {
+            let geometry = new THREE.BoxGeometry(10, 10, 10);
             let material = new THREE.MeshLambertMaterial({color: 'blue'});
-            let addPlayer = new THREE.Mesh(geometry, material);
-            scene.add(addPlayer);
-            addPlayer.position.set(x, y, 10);
+            let Player = new THREE.Mesh(geometry, material);
+            this.scene.add(Player);
+            Player.position.set(x, y, 10);
         }
 
-        var drawPlayers = function() {
+        this.drawPlayers = () => {
             for (let key in data.players) {
                     let x = data.players[key].x;
                     let y = data.players[key].y;
-                    addPlayer(x , y);
+                    this.addPlayer(-50 + x * 10 , -50 + y * 10);
             }
         }
 
-        var drawMap = () => {
+
+        this.drawMap = () => {
             let i = 0;
             for (let keyY in data.map) {
                 let j = 0;
                 for (let keyX in data.map[keyY]) {
                     if (data.map[keyY][keyX] < 10 ) {
-                        addBlock(-50 + j * 10, 0 + i * 10, 0 , 'green');    
-                    } else if (data.map[keyY][keyX] === 10) {addBlock(-50 + j * 10, 0 + i * 10, 10 , 'darkgreen');
-                                                            addBlock(-50 + j * 10, 0 + i * 10, 0 , 'green');}
-                      else {addBlock(-50 + j * 10, 0 + i * 10, 10 , 'grey');
-                            addBlock(-50 + j * 10, 0 + i * 10, 0 , 'green');}  
+                        this.addBlock(-50 + j * 10, 0 + i * 10, 0 , 'green');    
+                    } else if (data.map[keyY][keyX] === 10) {this.addBlock(-50 + j * 10, 0 + i * 10, 10 , 'darkgreen');
+                                                            this.addBlock(-50 + j * 10, 0 + i * 10, 0 , 'green');}
+                      else {this.addBlock(-50 + j * 10, 0 + i * 10, 10 , 'grey');
+                            this.addBlock(-50 + j * 10, 0 + i * 10, 0 , 'green');}  
                     j++;
                 }
                 i++;
             }
         }
 
-//--------------------------------------------------------------------------
-
-//---------------------------------------------Обработка света
-        const light_p = new THREE.PointLight(0xffffff);
-                light_p.position.set(10000, 10000, 10000);
-                scene.add(light_p);
-
-        const light_a = new THREE.AmbientLight(0x333333);
-                scene.add(light_a);
-//--------------------------------------------------------
-        
-//-------------------Анимация
-        const animate = () => {
-            requestAnimationFrame(animate);
-            renderer.render(scene, camera);
-
-        }
-//----------------------------------
-        
 
 
-        drawMap();
-        drawPlayers();
-        animate();
-
-
-        
+        this.drawMap();
+        this.drawPlayers();
     }
 
 
